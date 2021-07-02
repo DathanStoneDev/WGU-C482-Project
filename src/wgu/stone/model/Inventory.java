7d30a9aebe0e76af76b072cbc466db5e9e109c23
Dathan Stone <developer.stone@outlook.com>
2021-07-01 22:12:49 -0700

    Added the model classes for parts, products and inventory. Added their getters, setters and constructors.

---
+++ src/wgu/stone/model/Inventory.java
@@ -0,0 +1,12 @@
+package wgu.stone.model;
+
+import javafx.collections.ObservableList;
+
+
+public class Inventory {
+
+    private static ObservableList<Part> allParts;
+    private static ObservableList<Product> allProducts;
+
+
+}
---
+++ src/wgu/stone/model/Product.java
@@ -0,0 +1,72 @@
+package wgu.stone.model;
+
+import javafx.collections.ObservableList;
+
+public class Product {
+
+
+    private ObservableList<Part> associatedParts;
+    private int productId;
+    private String productName;
+    private double productPrice;
+    private int productStock;
+    private int minProduct;
+    private int maxProduct;
+
+    public Product(int productId, String productName, double productPrice, int productStock, int minProduct, int maxProduct) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productStock = productStock;
+        this.minProduct = minProduct;
+        this.maxProduct = maxProduct;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductStock() {
+        return productStock;
+    }
+
+    public void setProductStock(int productStock) {
+        this.productStock = productStock;
+    }
+
+    public int getMinProduct() {
+        return minProduct;
+    }
+
+    public void setMinProduct(int minProduct) {
+        this.minProduct = minProduct;
+    }
+
+    public int getMaxProduct() {
+        return maxProduct;
+    }
+
+    public void setMaxProduct(int maxProduct) {
+        this.maxProduct = maxProduct;
+    }
+}
